72b072a1d0eed14826ef4e1143e3bc97a13cfa29
zhangmingjing <247471195@qq.com>
2015-04-07 19:16:31 +0800

    share

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,25 +1,31 @@
 class WelcomeController < ApplicationController
   def index
-  	@channel_image = {
-  		"1"=>"child_cold.png",
-  		"2"=>"child_asthma.png",
-  		"3"=>"adult_cold.png",
-  		"4"=>"old_cold.png",
-  		"5"=>"lung_disease.png",
-  	}
-  	@channel_matter = {
-  		"1"=>"儿童感冒",
-  		"2"=>"儿童哮喘",
-  		"3"=>"成人感冒",
-  		"4"=>"老人感冒",
-  		"5"=>"慢性阻塞型肺病",
-  	}
-      @channel_aim = {
-              "1"=>"child_cold",
-              "2"=>"child_asthma",
-              "3"=>"adult_cold",
-              "4"=>"old_cold",
-              "5"=>"lung_disease",
+       @channel_id = {
+              "1"=>{
+                      image:"child_cold.png",
+                      matter:"儿童感冒",
+                      aim:"child_cold",
+              },
+              "2"=>{
+                      image:"child_asthma.png",
+                      matter:"儿童哮喘",
+                      aim:"child_asthma",
+              },
+              "3"=>{
+                      image:"adult_cold.png",
+                      matter:"成人感冒",
+                      aim:"adult_cold",
+              },
+              "4"=>{
+                      image:"old_cold.png",
+                      matter:"老人感冒",
+                      aim:"old_cold",
+              },
+              "5"=>{
+                      image:"lung_disease.png",
+                      matter:"慢性阻塞型肺病",
+                      aim:"lung_disease",
+              } ,
        }
   end
   def child_cold
